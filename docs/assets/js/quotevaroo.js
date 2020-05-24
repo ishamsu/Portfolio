5ee681b2c4842c83dcc9f1fb06ca06c7d49c6434
ishamsu <shamsu1997@gmail.com>
2020-05-24 12:47:58 +0530

    🎉 initial commit

---
+++ docs/assets/js/quotevaroo.js
@@ -0,0 +1,16 @@
+var quotes = [
+
+    "' മച്ചാൻ പൊളിയാണ് ' - imaginary user"
+
+]
+
+function newquote() {
+    var randomno = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quotedisplay').innerHTML = quotes[randomno];
+    document.getElementById('quotedisplay').style.color = randomColors();
+
+}
+
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
